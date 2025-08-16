4d5dd5883c6de449d9084d493d5591bb1b224a45
Emanuele Strano <ilanurs@gmail.com>
2025-08-16 12:19:16 +0200

    fix: update deployment paths in package.json and simplify adapter configuration in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,7 @@ import adapter from '@sveltejs/adapter-cloudflare';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// Configura Cloudflare Pages
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				experimentalJsonConfig: false
-			}
-		})
+		adapter: adapter()
 	}
 };
 
